1832b0e5cff3d13985de015d6ad9987145eaa884
DeveloperIgorR <bratca@inbox.ru>
2021-07-04 23:05:50 +0300

    Fix3

--- src/components/Todolistpanel.jsx
+++ src/components/Todolistpanel.jsx
@@ -1,27 +1,29 @@
 import { useState } from "react"
+const Todopanel = () => {
+    const [tasks, setTasks] = useState([])
+    const [taskText, setTaskText] = useState('')
+    let onButtonClick = () => {
+        setTasks([...tasks,taskText])
+    }
 
-const Todopanel = ()=>{
-    const  [tasks,setTasks] = useState([])
-    const [taskText,setTaskText] = useState('')
-    let onButtonClick = ()=>{
-        setTasks()
+    let onTextChange = ({ target: { value } }) => {
+        setTaskText(value)
     }
-    let onTextChange = (e)=>{
-        setTaskText(e.currentTarget.value)
-    }
-    let toDoListTasks = tasks.map(p=><li>{p.text}</li>)
-    return(
+    
+    let toDoListTasks = tasks.map(taskText => <li>{taskText}</li>)
+    
+    return (
         <div>
             <h1>ToDoList</h1>
             <div>
                 <div>
                     <ul>
-                      {toDoListTasks}
+                        {toDoListTasks}
                     </ul>
                 </div>
                 <div>
-                    <input placeholder ={'Подумай,тебе ведь это делать'} 
-                           onChange={onTextChange} value={taskText}></input>
+                    <input placeholder={'Задачи'}
+                        onChange={onTextChange} value={taskText}></input>
                 </div>
                 <div>
                     <button onClick={onButtonClick}>Add</button>
