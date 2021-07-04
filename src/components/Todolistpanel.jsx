a4a48c5c0efe623e9c443c432cfa032d5f66f44d
DeveloperIgorR <bratca@inbox.ru>
2021-07-04 19:44:03 +0300

    Fix2

--- src/components/Todolistpanel.jsx
+++ src/components/Todolistpanel.jsx
@@ -1,6 +1,33 @@
+import { useState } from "react"
+
 const Todopanel = ()=>{
+    const  [tasks,setTasks] = useState([])
+    const [taskText,setTaskText] = useState('')
+    let onButtonClick = ()=>{
+        setTasks()
+    }
+    let onTextChange = (e)=>{
+        setTaskText(e.currentTarget.value)
+    }
+    let toDoListTasks = tasks.map(p=><li>{p.text}</li>)
     return(
-        function(){}
+        <div>
+            <h1>ToDoList</h1>
+            <div>
+                <div>
+                    <ul>
+                      {toDoListTasks}
+                    </ul>
+                </div>
+                <div>
+                    <input placeholder ={'Подумай,тебе ведь это делать'} 
+                           onChange={onTextChange} value={taskText}></input>
+                </div>
+                <div>
+                    <button onClick={onButtonClick}>Add</button>
+                </div>
+            </div>
+        </div>
     )
 }
 export default Todopanel
