02b66cca7974c1831b50b8d763468f03746148ac
DeveloperIgorR <bratca@inbox.ru>
2021-07-10 00:12:33 +0300

    Fix15

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -7,27 +7,27 @@ const Users = (props) => {
     // const [userName, setUserName] = useState('')
     // const [userLastName, setUserLastName] = useState('')
 
-    const onButtonClick = () => {
-        // const fullUserName = userName + userLastName
-        // const fullName = {
-        //     id: Date.now(), title: fullUserName
-        // }
-        setUsers([...users, fullName])
-        // setUserName('')
-        // setUserLastName('')
-    }
+    // const onButtonClick = () => {
+    //     // const fullUserName = userName + userLastName
+    //     // const fullName = {
+    //     //     id: Date.now(), title: fullUserName
+    //     // }
+    //     setUsers([...users, fullName])
+    //     // setUserName('')
+    //     // setUserLastName('')
+    // }
 
     const onDelButtonClick = (person) => {
         const newUser = users.filter(item => item.id != person.id)
-        // setUsers(newUser)
+        setUsers(newUser)
     }
 
-    const onNameChange = ({ target: { value } }) => {
-        // setUserName(value)
-    }
-    const onLastNameChange = ({ target: { value } }) => {
-        // setUserLastName(value)
-    }
+    // const onNameChange = ({ target: { value } }) => {
+    //     // setUserName(value)
+    // }
+    // const onLastNameChange = ({ target: { value } }) => {
+    //     // setUserLastName(value)
+    // }
 
     return (
         <div>
@@ -35,8 +35,7 @@ const Users = (props) => {
               <Userslist users={users} onDelButtonClick={onDelButtonClick} />
             </div>
             <div>
-               <Usersdata users={users} onButtonClick={()=>onButtonClick} 
-               onNameChange={onNameChange} onLastNameChange={onLastNameChange}  />
+               <Usersdata />
             </div>   
         </div>
 
--- src/components/Users/Usersdata.jsx
+++ src/components/Users/Usersdata.jsx
@@ -2,23 +2,32 @@ import React from 'react'
 const Usersdata = (props) => {
     const [userName, setUserName] = useState('')
     const [userLastName, setUserLastName] = useState('')
-    let createUser = ()=>{
+    
+    const onButtonClick = () => {
+      let usersProfile = ()=>{
         const fullUserName = userName + userLastName
         const fullName = {
             id: Date.now(), title: fullUserName
         }
-        setUsers([...props.users, fullName])
-    }
+        console.log(usersProfile)
+    }  
+    const onNameChange = ({ target: { value } }) => {
+            setUserName(value)
+        }
+    const onLastNameChange = ({ target: { value } }) => {
+            setUserLastName(value)
+        }
     
+    }
     return (
         <div>
             <div>
-                <input placeholder='Имя' onChange={props.onNameChange(createUser)} value={userName} />
+                <input placeholder='Имя' onChange={onNameChange} value={userName} />
             </div>
             <div>
-                <input placeholder='Фамилия' onChange={props.onLastNameChange()} value={userLastName} />
+                <input placeholder='Фамилия' onChange={onLastNameChange} value={userLastName} />
             </div>
-            <button onClick={props.onButtonClick()}>Add full name</button>
+            <button onClick={onButtonClick}>Add full name</button>
         </div>
     )
 }
