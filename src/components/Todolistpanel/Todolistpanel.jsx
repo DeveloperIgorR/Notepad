de0a47d57e8658b356bf249a784a796e80a5bf75
DeveloperIgorR <bratca@inbox.ru>
2021-07-11 19:50:13 +0300

    Fix20

--- src/components/Todolistpanel/Todolistpanel.jsx
+++ src/components/Todolistpanel/Todolistpanel.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react"
+import s from './Todolistpanel.module.css'
 const Todopanel = () => {
     const [tasks, setTasks] = useState([])
     const [taskText, setTaskText] = useState('')
-
+    
+    const newTask = {
+        id: Date.now(), title: taskText,complited:false
+    }
     let onButtonClick = () => {
-        const newTask = {
-            id: Date.now(), title: taskText
-        }
+        
         setTasks([...tasks, newTask])
         setTaskText('')
     } 
@@ -20,15 +22,33 @@ const Todopanel = () => {
         setTaskText(value)
 
     }
-
+    let onFlagChange = ()=>{
+        <div className={s.checked}>
+            {newTask.title}
+        </div>
+    }
     let toDoListTasks = tasks.map(task => <li>
         <div>
-            <span>{task.title}</span><button onClick={()=>onButtonDelClick(task)}>Delete Task</button>
+            <span>{task.title}</span>
+            <input type='checkbox' id={task.id} onChange={onFlagChange}/>
+            <button onClick={()=>onButtonDelClick(task)}>Delete Task</button>
         </div>
     </li>)
-
+    // const[counter,setCounter] = useState(0)
+    
+    // let onButPlusclick = ()=>{
+              
+    //     setCounter(counter+1)
+    // }
+    
+    // let onButMinusClick = ()=>{
+        
+    //     setCounter(counter-1)
+    // }
     return (
         <div>
+            {/* <h1>{counter}</h1>
+            <button onClick={onButPlusclick}>+</button> <button onClick={onButMinusClick}>-</button>     */}
             <h2>ToDoList</h2>
             <div>
                 <div>
