4bc78cb84c06cd8a17c6941638679a28bd09b07c
DeveloperIgorR <bratca@inbox.ru>
2021-07-18 00:57:05 +0300

    Fix33

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = ()=> {
         <div className={n.navbar}>
          <div><NavLink to='/Todolistpanel' activeClassName={n.active}>Todopanel</NavLink> </div>
          <div><NavLink to='/Users' activeClassName={n.active}>Users</NavLink></div>
-         <div><NavLink to='/SinglePostPage' activeClassName={n.active}>SinglePostPage</NavLink></div>
+         <div><NavLink to={'posts/:id'} activeClassName={n.active}>SinglePostPage</NavLink></div>
          <div><NavLink to='/PostPage' activeClassName={n.active}>PostPage</NavLink></div>
         </div>
     )
--- src/components/PostPage/PostItem.jsx
+++ src/components/PostPage/PostItem.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 import s from './Usersposts.module.css'
 const PostItem = (props)=>{
     const [hovered,setHovered] = useState(false)
@@ -9,12 +10,14 @@ const PostItem = (props)=>{
         setHovered(false)
     }
 
+    
       return(
         <div onMouseEnter={onHover} onMouseLeave={offHover}
          className={hovered==true?s.hovered:s.unhovered}>
             <span>
-                <p>{props.currentPost.title}</p>
+            <NavLink to={'/posts/'+props.currentPost.id} ><p>{props.currentPost.title}</p></NavLink>
                 <button onClick={() => props.delPost(props.currentPost)}>DelPost</button>
+                
             </span>
         </div>
     )
