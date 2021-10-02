76378abe7b5a8b2701c4ce9d2070248ac06aec23
DeveloperIgorR <bratca@inbox.ru>
2021-10-03 00:04:10 +0300

    todo thunk

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import rootReducer from '../src/store/reducers'
+import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
